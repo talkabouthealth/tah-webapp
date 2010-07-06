44c915806d0cf6e33583b1444dc3812c45d8b375
kindcoder <kan.kangaroo@gmail.com>
2010-07-06 17:30:55 +0300

    Moved all static files

---
+++ app/controllers/Static.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Static extends Controller {
+
+    public static void aboutus() {
+        render();
+    }
+    
+    public static void communityGuidelines() {
+        render();
+    }
+    
+    public static void faq() {
+        render();
+    }
+    
+    public static void privacy() {
+        render();
+    }
+    
+    public static void tos() {
+        render();
+    }
+    
+}
