7f0f72176b3f4553d2c91913b6aaf7758d9ba462
Murray <murray@talkabouthealth.com>
2011-06-08 19:33:38 -0400

    new resource pages

--- app/controllers/Static.java
+++ app/controllers/Static.java
@@ -4,38 +4,92 @@ import play.mvc.Controller;
 
 public class Static extends Controller {
 	
-	public static void resources() {
-		render();
-	}
-	
-	public static void howitworks() {
-		render();
-	}
-	
+    public static void resources() {
+	render();
+    }
+    public static void howitworks() {
+	render();
+    }
     public static void aboutus() {
         render();
     }
-    
     public static void communityGuidelines() {
         render();
     }
-    
     public static void faq() {
         render();
     }
-    
     public static void privacy() {
         render();
     }
-    
     public static void tos() {
         render();
     }
-    
     public static void howyoucanhelp() {
     	render();
     }    
     public static void expertbenefits() {
     	render();
     }    
+    public static void resources_careerlegal() {
+    	render();
+    }    
+    public static void resources_beauty() {
+    	render();
+    }
+    public static void resources_chemotherapy() {
+    	render();
+    }
+    public static void resources_clinicaltrials() {
+    	render();
+    }    
+    public static void resources_emotionalsupport() {
+    	render();
+    }    
+    public static void resources_ethnicity() {
+    	render();
+    }    
+    public static void resources_familychildren() {
+    	render();
+    }    
+    public static void resources_finances() {
+    	render();
+    }    
+    public static void resources_gender() {
+    	render();
+    }    
+    public static void resources_generalhealth() {
+    	render();
+    }    
+    public static void resources_healthfitnessnutrition() {
+    	render();
+    }    
+    public static void resources_intimacysexuality() {
+    	render();
+    }    
+   public static void resources_lgbt() {
+    	render();
+    }    
+   public static void resources_newlydiagnosed() {
+    	render();
+    } 
+   public static void resources_patientadvocacy() {
+    	render();
+    } 
+   public static void resources_prevention() {
+    	render();
+    } 
+   public static void resources_reconstructivesurgery() {
+    	render();
+    } 
+   public static void resources_reproductive() {
+    	render();
+    } 
+   public static void resources_triplenegative() {
+    	render();
+    } 
+   public static void resources_youngsurvivors() {
+    	render();
+    } 
+
 }
