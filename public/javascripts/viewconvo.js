513f0a917809e8a5016036a184d655ae810f154e
avi <avibha@localhost.localdomain>
2011-12-28 18:43:18 +0530

    Changes for bug fixes and quantcast script

--- public/javascripts/viewconvo.js
+++ public/javascripts/viewconvo.js
@@ -15,6 +15,37 @@ function startWindow(){
 		$('.inline-edit').inlineEdit(
   		{ hover: '', saveFunction: saveData, updateFunction: updateData}
   	);
+	$(".flagConvoLink").click(function() {
+		flagType = "convo";
+		flagId = currentConvoId;
+		showPopup("#flagPopup", 200);
+	});
+	//Delete topic from topics (tags)
+	$(".deleteTopicLink").live("click", function() {
+		$(this).prev().remove();
+		var topicId = $(this).attr("rel");
+		$(this).remove();
+		deleteLinkClickEvent();
+		return false;
+	});
+	
+	$("#mergeConvosBtn").click(function() {
+  			if (selectedConvoURL) {
+  				var confirmMerge = confirm("Are you sure you want to merge the following question: "+selectedConvoURL);
+   	   		if (confirmMerge) {
+   	   			showStatus("Merging...");
+   	   			mergeConvoOp(selectedConvoURL);
+   	   		}
+  			} else {
+  	   			alert("Please select correct conversation");
+  			}
+		return false;
+	});
+	
+	$(".restartConvoLink").click(function() {
+		openChat(tid);
+		$.post("@{Conversations.restart}", {convoId: '${convo.id}'});
+	});			//----- Answers ------
 	makeTopicsAutocomplete("#topicInput");
    	makeAutocomplete("#relatedConvosInput", "convoedit");
    	makeAutocomplete("#followupConvosInput", "convoedit");
