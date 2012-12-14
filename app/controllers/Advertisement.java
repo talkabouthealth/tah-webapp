56040ecd49333175a9e3af979bc58114adec22d3
avi <avibha@localhost.localdomain>
2012-12-14 11:32:28 +0530

    changes for ad reprot feature

--- app/controllers/Advertisement.java
+++ app/controllers/Advertisement.java
@@ -1,9 +1,19 @@
 package controllers;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.TalkerBean;
+
 import org.apache.commons.lang.StringUtils;
 
 import dao.AdvertisementDAO;
+import dao.NewsLetterDAO;
 import play.mvc.Controller;
+import util.CommonUtil;
 
 public class Advertisement extends Controller {
 
@@ -22,5 +32,54 @@ public class Advertisement extends Controller {
 			AdvertisementDAO.populateStats(addId, "click",  true);
 		}
 		redirect("http://www.fifthseasonfinancial.com/");
+	}
+	
+	public static void logReport(String fromDate,String toDate) {
+		
+		if (Security.isConnected()) {
+			TalkerBean talker = CommonUtil.loadCachedTalker(session);
+			if(talker.isAdmin()) {
+				String errorMsg = "No/Wrong date range will display complete stats";
+				Map<String, String> emailList = new HashMap<String, String>();
+				DateFormat dateFormat = new SimpleDateFormat("yyyy-dd-MM");//09/04/2012
+				Date fromDt = new Date();
+				Date toDt = new Date();
+				long total = 0;
+				
+				boolean dateError = false;
+				if(fromDate != null && !"".equals(fromDate)) {
+					try {
+						fromDt = dateFormat.parse(fromDate);
+						toDt = dateFormat.parse(toDate);
+						if(toDt.before(fromDt)){
+							dateError = true;
+						}
+					} catch(Exception e) {
+						e.printStackTrace();
+						dateError = true;
+					}
+					if(dateError){
+						errorMsg = "Wrong dates selected";
+					}
+				}else{
+					dateError = true;
+				}
+				
+				
+				if(!dateError){
+					emailList = AdvertisementDAO.getAdvertisementCount(fromDt,toDt);
+				}else{
+					emailList = AdvertisementDAO.getAdvertisementCount(null,null);
+				}
+				total = Long.parseLong(emailList.get("all"));
+				emailList.remove("all");
+				
+				render(emailList,total,fromDate,toDate,errorMsg);
+			} else {
+				forbidden();
+			}
+		} else {
+			forbidden();
+		}
 	}
 }
